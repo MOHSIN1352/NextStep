f2092790230d74455e5e9b911727815b60582f8a
Mohsin Pathan <106861993+MOHSIN1352@users.noreply.github.com>
2025-03-13 19:42:05 +0530

    Delete Frontend NextStep/backend/models/cityModel.js
    double

--- Frontend NextStep/backend/models/cityModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const citySchema = new mongoose.Schema({
-    City_ID: { type: mongoose.Schema.Types.ObjectId, auto: true },  // Auto-generated unique ID
-    City_Name: { type: String, required: true },
-    State_ID: { type: mongoose.Schema.Types.ObjectId, ref: "State", required: true }  // Foreign key reference to State model
-});
-
-module.exports = mongoose.model("City", citySchema);
