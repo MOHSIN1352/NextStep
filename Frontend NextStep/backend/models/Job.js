97eb9a8e598dd9ab7ffe01db996685e20a246947
Annixpratap <202412008@daiict.ac.in>
2025-03-30 01:18:50 +0530

    Update Job.js

--- Frontend NextStep/backend/models/Job.js
+++ Frontend NextStep/backend/models/Job.js
@@ -1,43 +1,28 @@
-const mongoose = require('mongoose');
-const AutoIncrement = require('mongoose-sequence')(mongoose);
+const mongoose = require("mongoose");
 
-const courseSchema = new mongoose.Schema({
-  Name: {
+const jobSchema = new mongoose.Schema({
+  Title: {
     type: String,
     required: true
   },
-  Platform: {
+  Company_Name: {
     type: String,
     required: true
   },
-  Category: {
+  Industry_Type: {
     type: String,
     required: true
   },
-  url: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  duration: {
-    type: String, // Consider using Number if duration is in hours/days
-    required: true
-  },
-  certification: {
-    type: Boolean,
-    required: true
-  },
-  rating: {
+  Salary: {
     type: Number,
     required: true,
-    min: 0,
-    max: 5
+    min: 0 
+  },
+  Location: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'City',
+    required: true
   }
 });
 
-// Apply auto-increment plugin for Course_ID
-courseSchema.plugin(AutoIncrement, { inc_field: "Course_ID", start_seq: 1 });
-
-const Course = mongoose.model('Course', courseSchema);
-
-module.exports = Course;
+module.exports = mongoose.model("Job", jobSchema);
